95f91b63cff4bfb1c13a72077346cc94cbfce2df
CharlieHii <charlie_chaper@hotmail.com>
2024-07-17 15:07:03 +0700

    feat: complete all assignments

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,50 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let ceilIndex = books.length - 1;
+  let floorIndex = 0;
+  let currentIndex;
+
+  const setCurrentIndex = () => {
+    currentIndex = Math.floor((ceilIndex + floorIndex) / 2);
+  };
+  setCurrentIndex();
+
+  while (ceilIndex >= floorIndex) {
+    if (searchTitle === books[currentIndex].title) {
+      return currentIndex;
+    }
+    if (searchTitle > books[currentIndex].title) {
+      floorIndex = currentIndex + 1;
+      setCurrentIndex();
+    }
+    if (searchTitle < books[currentIndex].title) {
+      ceilIndex = currentIndex - 1;
+      setCurrentIndex();
+    }
+  }
+
+  return -1;
 }
+
+let books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+let searchBook = "Gone with the Wind";
+
+console.log(
+  "test case 1: ",
+  findBookIndex(books, searchBook) === 6 ? "Approve" : "Reject"
+);
+
+searchBook = "The Master Mind";
+console.log(
+  "test case 2: ",
+  findBookIndex(books, searchBook) === -1 ? "Approve" : "Reject"
+);
